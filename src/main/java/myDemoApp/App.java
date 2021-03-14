27088899468b8d00bdd58cfaa8d75ed05a6eee83
Kandahar Ozturk <kandaharozturk@etu.edu.tr>
2021-03-14 16:59:00 +0300

    Change port part

--- src/main/java/myDemoApp/App.java
+++ src/main/java/myDemoApp/App.java
@@ -38,11 +38,7 @@ public class App {
     public static void main(String[] args) {
         port(getHerokuAssignedPort());
 
-        Logger logger = LogManager.getLogger(App.class);
-
-        int port = Integer.parseInt(System.getenv("PORT"));
-        port(port);
-        logger.error("Current port number:" + port);
+        
 
 
         get("/", (req, res) -> "Hello, World");
